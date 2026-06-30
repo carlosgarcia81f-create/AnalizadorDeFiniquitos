05b8e64ce6353ce4ef5c32df51c2eb492f07273a
carlosgarcia81f-create <carlosgarcia81f@gmail.com>
2026-06-30 10:47:34 -0600

    Update streamlit_app.py
    Se han hecho intentos para implementar Reglas de Negocio. Fallido, no está haciendo la selección de conceptos por implementar eso.

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from st_aggrid import AgGrid, GridOptionsBuilder
 # ------------ C O N F I G U R A C I Ó N  D E  P Á G I N A -------------------------------------------------------------#
 # 1. Título
 st.title("Analizador de Finiquitos")
-
+st.write("CÓDIGO EN REPARACIÓN")
 # 2. Controles laterales (lo que eran tus variables de @param)
 filas_a_saltar = st.sidebar.number_input("Filas a saltar", value=11)
 nombre_hoja = st.sidebar.text_input("Nombre de la hoja", value="12")
